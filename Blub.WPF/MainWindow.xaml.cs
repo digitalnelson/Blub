0de17293c92f2932a4592e8671e6a6ab6ff9af04
Brent G Nelson, MD <brent@digitalnelson.com>
2017-10-29 10:32:16 -0500

    first check in

---
+++ Blub.WPF/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Blub.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SerialPort port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+            port.Open();
+            port.Write(new char[] { 'r', 'w', (char)0x00 }, 0, 3);
+            port.Write(new char[] { 'r', 'b', (char)0x00 }, 0, 3);
+
+            //port.Write("r");
+            //port.Write("B");
+            //port.Write(" ");
+
+            port.Close();
+        }
+    }
+}
